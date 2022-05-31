4f06e1f429e21f10d699d3cddc9084aa8857ff77
SarthakKeshari <srthkkeshari84@gmail.com>
2022-06-01 01:25:52 +0530

    Automating

--- automate.js
+++ automate.js
@@ -16,29 +16,32 @@ function execute(command, callback){
 execute('git checkout -b auto'+Date.now(), (q) => {
     console.log("Branch Created\n", q)
     let data = JSON.parse(fs.readFileSync('package.json', "utf-8"))
-    data.dependencies.nodemon = "^2.2.12"
+    data.dependencies.nodemon = "^2.0.10"
     // console.log(data)
     fs.writeFileSync('package.json', JSON.stringify(data))
     console.log("Package.json updated")
-    execute('git add -A', (a) => {
-        console.log("Add\n",a)
-        execute('git commit -m "Automating"', (b) => {
-            console.log("2 Commit\n",b)
-            execute('git symbolic-ref --short HEAD', (c) => {
-                console.log("3 Branch name\n",c)
-                const branch_name = c;
-                execute('git push origin '+ branch_name, (d) => {
-                    console.log("4 Push\n", d)
-                    execute('git config --get remote.upstream.url', (e) => {
-                        const cloned_repo = e.replace(/\n/g, '')
-                        console.log("5 Upstream Repo\n", e)
-                        execute('git config --get remote.origin.url', (f) => {
-                            const my_repo = f
-                            const my_username = my_repo.split('/')[3]
-                            console.log("6 My Repo\n", f)
-                            console.log("6 My Username\n", my_username)
-                            execute('gh pr create --title "Automated Pull" --body "Automated Pull Testing" --repo '+cloned_repo+' --head '+my_username+':'+branch_name, (g) => {
-                                console.log("7 PR generated\n", g)
+    execute('npm i', (w) => {
+        console.log("NPM Installed\n", w)
+        execute('git add -A', (a) => {
+            console.log("Add\n",a)
+            execute('git commit -m "Automating"', (b) => {
+                console.log("2 Commit\n",b)
+                execute('git symbolic-ref --short HEAD', (c) => {
+                    console.log("3 Branch name\n",c)
+                    const branch_name = c;
+                    execute('git push origin '+ branch_name, (d) => {
+                        console.log("4 Push\n", d)
+                        execute('git config --get remote.upstream.url', (e) => {
+                            const cloned_repo = e.replace(/\n/g, '')
+                            console.log("5 Upstream Repo\n", e)
+                            execute('git config --get remote.origin.url', (f) => {
+                                const my_repo = f
+                                const my_username = my_repo.split('/')[3]
+                                console.log("6 My Repo\n", f)
+                                console.log("6 My Username\n", my_username)
+                                execute('gh pr create --title "Automated Pull" --body "Automated Pull Testing" --repo '+cloned_repo+' --head '+my_username+':'+branch_name, (g) => {
+                                    console.log("7 PR generated\n", g)
+                                })
                             })
                         })
                     })
