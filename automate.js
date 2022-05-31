91f1f8c9004f5a2fa96f49a190e41e53c0b57eae
SarthakKeshari <srthkkeshari84@gmail.com>
2022-05-31 23:28:02 +0530

    Automating

--- automate.js
+++ automate.js
@@ -1,27 +1,47 @@
 const exec = require('child_process').exec;
 
-exec('git add -A', function(err, stdout, stdder)
-{
-    console.log("1")
-    console.log(stdout)
-    exec('git commit -m "Automating"', function(err, stdout, stdder)
-    {   
-        console.log("2")
-        console.log(stdout)
-        let curr_branch = null
-        exec('git symbolic-ref --short HEAD', function(err, stdout, stdder)
-        {
-            console.log(curr_branch)
-            curr_branch = stdout
-            exec('git push origin '+curr_branch, function(err, stdout, stdder)
-            {
-                console.log("4")
-                console.log(stdout)
-            })
-        })
+function execute1(command, callback){
+    exec(command, function(error, stdout, stderr){ 
+        callback(stdout); 
+    });
+};
 
+function execute2(command, callback){
+    exec(command, function(error, stdout, stderr){ 
+        callback(stdout); 
+    });
+};
+
+function execute3(command, callback){
+    exec(command, function(error, stdout, stderr){ 
+        callback(stdout); 
+    });
+};
+
+execute1('git add -A', (a) => {
+    console.log("1",a)
+    execute2('git commit -m "Automating"', (b) => {
+        console.log("2",b)
+        execute3('git symbolic-ref --short HEAD', (c) => {
+            console.log("3",c)
+        })
     })
 })
+
+
+// .then(() => {
+// let curr_branch = null
+// exec('git symbolic-ref --short HEAD', function(err, stdout, stdder)
+// {
+//     console.log(curr_branch)
+//     curr_branch = stdout
+// })})
+// .then(
+// exec('git push origin '+curr_branch, function(err, stdout, stdder)
+// {
+//     console.log("4")
+//     console.log(stdout)
+// }))
 
 
 
