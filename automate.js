91e6f062f6eda4fa24a1ba640e9b67e6579da01b
SarthakKeshari <srthkkeshari84@gmail.com>
2022-05-31 23:44:44 +0530

    Automating

--- automate.js
+++ automate.js
@@ -1,37 +1,20 @@
 const exec = require('child_process').exec;
 
-function execute1(command, callback){
+function execute(command, callback){
     exec(command, function(error, stdout, stderr){ 
         callback(stdout); 
     });
 };
 
-function execute2(command, callback){
-    exec(command, function(error, stdout, stderr){ 
-        callback(stdout); 
-    });
-};
-
-function execute3(command, callback){
-    exec(command, function(error, stdout, stderr){ 
-        callback(stdout); 
-    });
-};
-
-function execute4(command, callback){
-    exec(command, function(error, stdout, stderr){ 
-        callback(stdout); 
-    });
-};
-
-execute1('git add -A', (a) => {
+execute('git add -A', (a) => {
     console.log("1",a)
-    execute2('git commit -m "Automating"', (b) => {
+    execute('git commit -m "Automating"', (b) => {
         console.log("2",b)
-        execute3('git symbolic-ref --short HEAD', (c) => {
+        execute('git symbolic-ref --short HEAD', (c) => {
             console.log("3",c)
-            execute4('git push origin '+c, (d) => {
+            execute('git push origin '+c, (d) => {
                 console.log("4", d)
+
             })
         })
     })
